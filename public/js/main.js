b680da6556cd6d5b13feed3a042e96f29f46c028
vinitsarode <vinit.sarode12@gmail.com>
2020-10-06 20:24:29 +0530

    testimonials section added

--- public/js/main.js
+++ public/js/main.js
@@ -47,3 +47,29 @@ if ($('.navbar').length > 0) { // check if element exists
     });
 }
 
+
+// ========== Testimonials carousel =============
+
+$(function() {
+    "use strict";
+    $('#customers-testimonials').owlCarousel({
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
